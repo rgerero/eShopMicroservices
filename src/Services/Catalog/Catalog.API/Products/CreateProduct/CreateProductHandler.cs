4917d9b8c9894d93b1d440aaa25df87cc9fa8f89
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-13 16:25:54 +0800

    add and implement fluent validation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,34 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct
 {
 	public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal price)
 		: ICommand<CreateProductResult>;
 	public record CreateProductResult(Guid Id);
-	internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+	public class CreateProductCommandValidator:AbstractValidator<CreateProductCommand>
+	{
+		public CreateProductCommandValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+			RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+			RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+			RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+			RuleFor(x => x.price).GreaterThan(0).WithMessage("Price must be greater than 0");
+		}
+	}
+	internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) 
+		: ICommandHandler<CreateProductCommand, CreateProductResult>
 	{
 		public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
 		{
-			//to do
-			//create product entity
+			var result=await validator.ValidateAsync(command, cancellationToken);
+			var error=result.Errors.Select(x=>x.ErrorMessage).ToList();
+			if (error.Any())
+			{
+				throw new ValidationException(error.FirstOrDefault());
+			}
+
 			var product = new Product
 			{
 				Name = command.Name,
