71ab65cc75c3a7c8eac477b00f58cee3d4ee2c19
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-18 23:44:46 +0800

    implement health checks for api and postgres db

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,11 +25,18 @@ if(builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+	.AddNpgSql(builder.Configuration.GetConnectionString("DB")!);
 
 var app = builder.Build();
 
 //configure http request pipeline
 app.MapCarter();
 app.UseExceptionHandler(opton => { });
+app.UseHealthChecks("/health",
+	new HealthCheckOptions
+	{
+		ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
+	});
 
 app.Run();
