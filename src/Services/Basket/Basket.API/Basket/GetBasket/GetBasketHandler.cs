eb0a7fa5c193f6e9d3f1d87f74ae06e72f256a0e
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-20 01:38:37 +0800

    Add model, global usings, and add mplementation for Getbasket classes.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,17 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-	public class GetBasketHandler
+	public record GetBasketQuery(string UserName):IQuery<GetBasketResult>;
+
+	public record GetBasketResult(ShoppingCart Cart);
+
+	public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 	{
+		public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+		{
+			//to do:
+			//get data in the database
+
+			return new GetBasketResult(new ShoppingCart());
+		}
 	}
 }
