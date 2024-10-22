572cd1944aa6b4f9ad34b01e829aa3300d29f571
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-22 21:52:32 +0800

    Create Basket repository class

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Basket.API.Data
+{
+	public class BasketRepository : IBasketRepository
+	{
+
+		public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellation = default)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellation = default)
+		{
+			throw new NotImplementedException();
+		}
+		public Task<bool> DeleteBasket(string userName, CancellationToken cancellation = default)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
