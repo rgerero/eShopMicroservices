da2279dde493fed831f5aa50ab385ac04be1df5e
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-29 20:52:58 +0800

    [discount] - configure discountGrpc service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -12,7 +12,7 @@ if(app.Environment.IsDevelopment())
 	app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
