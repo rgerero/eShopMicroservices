b23dac475d1edc5b8fc279288c33ef2b60bc058c
rgerero <31179503+rgerero@users.noreply.github.com>
2024-10-29 21:40:03 +0800

    [discount] - Seed and auto-migrate sqllitedb using ef core

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -18,6 +18,7 @@ if(app.Environment.IsDevelopment())
 	app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
