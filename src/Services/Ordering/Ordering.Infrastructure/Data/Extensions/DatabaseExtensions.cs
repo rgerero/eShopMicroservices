c008aa4423c3fc10d460c8a30e57e50c9b97ebf9
rgerero <31179503+rgerero@users.noreply.github.com>
2024-11-24 09:44:11 +0800

    [ordering] - seed customers, products, & orders in sql OrderDb using EF core when app start.

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -10,6 +10,40 @@ namespace Ordering.Infrastructure.Data.Extensions
 			using var scope=app.Services.CreateScope();
 			var context =scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
 			context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+			await SeedAsync(context);
+		}
+
+		private static async Task SeedAsync(ApplicationDBContext context)
+		{
+			await SeedCustomerAsync(context);
+			await SeedProductAsync(context);
+			await SeedOrderandItemsAsync(context);
+		}
+
+		private static async Task SeedCustomerAsync(ApplicationDBContext context)
+		{
+			if (!await context.Customers.AnyAsync())
+			{
+				await context.Customers.AddRangeAsync(InitialData.Customers);
+				await context.SaveChangesAsync();
+			}
+		}
+		private static async Task SeedProductAsync(ApplicationDBContext context)
+		{
+			if (!await context.Products.AnyAsync())
+			{
+				await context.Products.AddRangeAsync(InitialData.Products);
+				await context.SaveChangesAsync();
+			}
+		}
+		private static async Task SeedOrderandItemsAsync(ApplicationDBContext context)
+		{
+			if (!await context.Orders.AnyAsync())
+			{
+				await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+				await context.SaveChangesAsync();
+			}
 		}
 	}
 }
