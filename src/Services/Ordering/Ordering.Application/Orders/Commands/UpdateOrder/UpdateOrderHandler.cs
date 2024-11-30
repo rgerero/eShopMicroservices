29a32cd8a6c0354f975f5a8ee68d3e4a4b9cbc9e
rgerero <31179503+rgerero@users.noreply.github.com>
2024-11-30 16:16:56 +0800

    [ordering] - remove the nullable param when updating new order.

--- src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -27,7 +27,7 @@ namespace Ordering.Application.Orders.Commands.UpdateOrder
 			return new UpdateOrderResult(true);
 		}
 
-		private void UpdateOrderWithNewValues(Order? order, OrderDto orderDto)
+		private void UpdateOrderWithNewValues(Order order, OrderDto orderDto)
 		{
 			var shippingAddress = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
 			var billingAddress = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
