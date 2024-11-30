5d2d743d276494037744faa8c0d8389dbddd8eb5
rgerero <31179503+rgerero@users.noreply.github.com>
2024-11-30 15:44:21 +0800

    [ordering] - develop orders CreateOrderHandler using IApplicationDbContext

--- src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -1,4 +1,4 @@
 ﻿namespace Ordering.Application.Dtos
 {
-	public record OrderItemDto(Guid Id, Guid CustomerId, int Quantity, decimal Price);
+	public record OrderItemDto(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
 }
