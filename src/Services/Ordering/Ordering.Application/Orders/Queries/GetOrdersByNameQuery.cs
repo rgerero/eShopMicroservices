aaf4af40b96f8c7fcea8eb27988de52f14dbc442
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-02 21:39:00 +0800

    [ordering] - implement event handlers, develop query handler and extensions with cqrs and MediatR

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByNameQuery.cs
@@ -0,0 +1,7 @@
+﻿using Ordering.Domain.Models;
+
+namespace Ordering.Application.Orders.Queries
+{
+	public record GetOrdersByNameQuery(string Name):IQuery<GetOrdersByNameResult>;
+	public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
+}
