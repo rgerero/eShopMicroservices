dd7e7b36797e0e5e9202b5526a5794209f2116c2
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-09 00:48:49 +0800

    [ordering] - install carter tool and develop createOrder endpoint.

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,6 +1,31 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints
 {
-	public class CreateOrder
+
+	public record CreateOrderRequest(OrderDto Order);
+	public record CreateOrderResponse(Guid Id);
+	public class CreateOrder : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+			{
+				var command = request.Adapt<CreateOrderCommand>();
+				var result = await sender.Send(command);
+				var response = result.Adapt<CreateOrderResponse>();
+
+				return Results.Created($"/orders/{response.Id}", response);
+			})
+			.WithName("CreateOrder")
+			.Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Create Order")
+			.WithDescription("Create Order");
+		}
 	}
 }
