f0d6cb3649e961fd098c0357f880ea4bb2618b00
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-09 14:55:34 +0800

    [ordering] - develop GetOrdersByName endpoint for REST APIs in Ordering.API layer

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,6 +1,28 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints
 {
-	public class GetOrdersByName
+	public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+	public class GetOrdersByName : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+			{
+				var result=await sender.Send(new GetOrdersByNameQuery(orderName));
+				var response=result.Adapt<GetOrdersByNameResponse>();
+				
+				return Results.Ok(response);
+			})
+			.WithName("GetOrdersByName")
+			.Produces<GetOrdersByNameResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Get Orders By Name")
+			.WithDescription("Get Orders By Name");
+		}
 	}
 }
