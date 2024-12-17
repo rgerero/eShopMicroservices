a56c2f20d3c5074365e639728b6dce67368e40e5
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-17 08:37:50 +0800

    [ordering] - develop the getOrders endpoint with pagination for REST APIs in ordering.api layer

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,23 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
-	public class GetOrders
+	public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+	public class GetOrders : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("/orders/", async ([AsParameters] PaginationRequest request, ISender sender) =>
+			{
+				var result = await sender.Send(new GetOrdersQuery(request));
+				var responose=result.Adapt<GetOrdersResponse>();
+				return Results.Ok(responose);
+			});
+		}
 	}
 }
