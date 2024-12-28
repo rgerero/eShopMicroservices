e6238a4617dcb6176005145a704ad2c3365f913d
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-28 23:29:07 +0800

    Create buildingBlock messaging class for events and massTransit class files.

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit
+{
+	public static class Extensions
+	{
+		public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly=null)
+		{
+			return services;
+		}
+	}
+}
