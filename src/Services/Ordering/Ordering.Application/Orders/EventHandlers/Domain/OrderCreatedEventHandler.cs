6b2a5353e510735947667e4e8da1c115767702dc
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-29 23:09:58 +0800

    [ordering] - Publish orderCreated integration event for Order fullfilment microservices

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,15 +1,22 @@
-﻿using MediatR;
+﻿using Mapster;
+using MassTransit;
+using MediatR;
 using Microsoft.Extensions.Logging;
+using Ordering.Application.Orders.Extensions;
 using Ordering.Domain.Events;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    public class OrderCreatedEventHandler(IPublishEndpoint pubEndpoint,ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Doman Event handled: {DomainEvent}", notification.GetType().Name);
-            return Task.CompletedTask;
+            logger.LogInformation("Doman Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+            await pubEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+
         }
     }
 }
