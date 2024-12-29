6ccb781c34039f93b3e3935fecf5a4a56fbdd93d
rgerero <31179503+rgerero@users.noreply.github.com>
2024-12-29 22:46:55 +0800

    [orderig] - Develop basket checkout event consumer integration handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    {
+        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Doman Event handled: {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
